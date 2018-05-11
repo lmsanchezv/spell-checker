211eba5ee3ccaf0997373c48c2a91a9a1d9eeb63
spartanjoax <eldj.joax@gmail.com>
2018-05-11 12:04:45 -0600

    UnigramasMod

--- Unigrams.py
+++ Unigrams.py
@@ -1,48 +1,48 @@
 import math
 
-# used for unseen words in training vocabularies
+# Usado para palabras que no existen en el vocabulario de entrenamiento
 UNK = None
 
-# sentence start and end
-SENTENCE_START = "<s>"
-SENTENCE_END = "</s>"
+# inicio y fin de oración
+inicioOracion = "<s>"
+finOracion = "</s>"
 
-class UnigramLanguageModel:
-    def __init__(self, sentences, smoothing=False):
-        self.unigram_frequencies = dict()
-        self.corpus_length = 0
-        for sentence in sentences:
-            for word in sentence:
-                self.unigram_frequencies[word] = self.unigram_frequencies.get(word, 0) + 1
-                if word != SENTENCE_START and word != SENTENCE_END:
-                    self.corpus_length += 1
-        # subtract 2 because unigram_frequencies dictionary contains values for SENTENCE_START and SENTENCE_END
-        self.unique_words = len(self.unigram_frequencies) - 2
+class UnigramaModeloLenguaje:
+    def __init__(self, oraciones, smoothing=False):
+        self.frecuenciasUnigramas = dict()
+        self.tamanoCorpus = 0
+        for oracion in oraciones:
+            for palabra in oracion:
+                self.frecuenciasUnigramas[palabra] = self.frecuenciasUnigramas.get(palabra, 0) + 1
+                if palabra != inicioOracion and palabra != finOracion:
+                    self.tamanoCorpus += 1
+        # Resta 2 porque el diccionari ofrecuenciasUnigramas contiene valores de inicioOracion y finOracion
+        self.palabrasUnicas = len(self.frecuenciasUnigramas) - 2
         self.smoothing = smoothing
 
-    def calculate_unigram_probability(self, word):
-            word_probability_numerator = self.unigram_frequencies.get(word, 0)
-            word_probability_denominator = self.corpus_length
+    def calcularProbabilidadUnigrama(self, palabra):
+            numeradorProbabilidadPalabra = self.frecuenciasUnigramas.get(palabra, 0)
+            denominadorProbabilidadPalabra = self.tamanoCorpus
             if self.smoothing:
-                word_probability_numerator += 1
-                # add one more to total number of seen unique words for UNK - unseen events
-                word_probability_denominator += self.unique_words + 1
-            return float(word_probability_numerator) / float(word_probability_denominator)
+                numeradorProbabilidadPalabra += 1
+                # agregar uno más al total de palabras vistas para el UNK
+                denominadorProbabilidadPalabra += self.palabrasUnicas + 1
+            return float(numeradorProbabilidadPalabra) / float(denominadorProbabilidadPalabra)
 
-    def calculate_sentence_probability(self, sentence, normalize_probability=True):
-        sentence_probability_log_sum = 0
-        for word in sentence:
-            if word != SENTENCE_START and word != SENTENCE_END:
-                word_probability = self.calculate_unigram_probability(word)
-                sentence_probability_log_sum += math.log(word_probability, 2)
-        return math.pow(2, sentence_probability_log_sum) if normalize_probability else sentence_probability_log_sum                
+    def calcularProbabilidadOracion(self, oracion, normalizarProbabilidad=True):
+        probabilidadLogOracion = 0
+        for palabra in oracion:
+            if palabra != inicioOracion and palabra != finOracion:
+                palabra_probability = self.calcularProbabilidadUnigrama(palabra)
+                probabilidadLogOracion += math.log(palabra_probability, 2)
+        return math.pow(2, probabilidadLogOracion) if normalizarProbabilidad else probabilidadLogOracion                
 
-    def sorted_vocabulary(self):
-        full_vocab = list(self.unigram_frequencies.keys())
-        full_vocab.remove(SENTENCE_START)
-        full_vocab.remove(SENTENCE_END)
-        full_vocab.sort()
-        full_vocab.append(UNK)
-        full_vocab.append(SENTENCE_START)
-        full_vocab.append(SENTENCE_END)
-        return full_vocab
+    def obtenerVocabularioOrdenado(self):
+        vocabularioCompleto = list(self.frecuenciasUnigramas.keys())
+        vocabularioCompleto.remove(inicioOracion)
+        vocabularioCompleto.remove(finOracion)
+        vocabularioCompleto.sort()
+        vocabularioCompleto.append(UNK)
+        vocabularioCompleto.append(inicioOracion)
+        vocabularioCompleto.append(finOracion)
+        return vocabularioCompleto
