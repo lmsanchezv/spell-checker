aae18bc56e50479dda462daad071eb420afdf1f4
spartanjoax <eldj.joax@gmail.com>
2018-05-22 21:35:04 -0600

    V2 de Crear matrices de acuerdo a nuevo Excel pero con error de encoding

---
+++ CreateMatrixFromExcelv2.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import json, re
+from collections import defaultdict
+
+filesNames = ['matrixInsert.data','matrixDelete.data','matricesubs.data','matrixExchange.data']
+allData = [defaultdict(list),defaultdict(list),defaultdict(list),defaultdict(list)]
+data = []
+df = pd.read_excel('matrices/matrices.xls')
+matrix  = df.as_matrix()
+for row in matrix[1:]:
+    i = -1
+    if row[2] == "Substitucion":
+        i = 2
+    elif row[2] == "Borrado":
+        i = 1
+    elif row[2] == "Insercion":
+        i = 0
+    elif row[2] == "Intercambio":
+        i = 3
+
+    if i >= 0:
+        if row[3]+','+row[4] in allData[i]:
+            allData[i][row[3]+','+row[4]] += 1
+        else:
+            allData[i][row[3]+','+row[4]] = 1
+        print allData[i][row[3]+','+row[4]]
+
+for fil in range(len(filesNames)):
+    json_data = json.dumps(allData[fil], ensure_ascii=False)
+    with open('matrices/%s' % filesNames[fil], 'w') as f:
+        f.write(json_data)
